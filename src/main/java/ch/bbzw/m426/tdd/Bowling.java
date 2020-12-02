b87ce3ff4f4920b763d95bce5a08d0be2bb84dd0
the-philip-baumann <philip.baumann@hispeed.ch>
2020-12-02 09:17:22 +0100

    Implement IBowling methods and wrote first step test

---
+++ src/main/java/ch/bbzw/m426/tdd/Bowling.java
@@ -0,0 +1,16 @@
+package ch.bbzw.m426.tdd;
+
+
+public class Bowling implements IBowling {
+
+
+    @Override
+    public void score(String score) {
+
+    }
+
+    @Override
+    public int getResult() {
+        return 0;
+    }
+}
--- src/test/java/ch/bbzw/m426/tdd/BowlingTest.java
+++ src/test/java/ch/bbzw/m426/tdd/BowlingTest.java
@@ -1,4 +1,16 @@
 package ch.bbzw.m426.tdd;
 
-public class BowlingTest {
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class BowlingTest  {
+    @Test
+    public void getResult_score0Point_0() {
+        IBowling testee = new Bowling();
+        int expected = 0;
+
+        int actual = testee.getResult();
+
+        assertEquals(expected, actual);
+    }
 }
